e2bb3727e7ecd2dd564eafc1722920cec0e8a544
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2022-09-21 09:25:08 +0200

    Stream checking

--- src/controller/stream.controller.js
+++ src/controller/stream.controller.js
@@ -5,7 +5,7 @@ const express = require("express"),
   logger = require("../util/logging"),
   users = require("../domain/services/user.service");
 
-console.log("[[ STREAM ]]");
+console.log("[[ STREAM Checking]]");
 logger.LogInfo("[GET] = /api/v1/stream/");
 logger.LogInfo("[GET] = /api/v1/stream/:userID");
 logger.LogInfo("[GET] = /api/v1/stream/:userID/:deviceID");
